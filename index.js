543f3d5e1a1273eb8c21c8835f3808e3d908c0e3
codingvaleria <codingvaleria@gmail.com>
2022-08-17 16:29:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+// function declaration
+function saturdayFun(activity = "roller-skate") {
+
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log((saturdayFun("dance")));
+
+
+// function expression
+
+const mondayWork = function (todo = "go to the office") {
+    return `This Monday, I will ${todo}.`
+}
+console.log(mondayWork("apply for VISA"));
+
+function wrapAdjective(visualflair = "*") {
+
+    const innerFunction = function (nameMe = "special") {
+
+        return `You are ${visualflair}${nameMe}${visualflair}!`;
+    };
+
+    return innerFunction;
+
+}
